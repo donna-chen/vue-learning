c61bcd1c76dbee5efee6d2e91fdd7640862ffbb2
donna-chen <422194155@qq.com>
2020-03-17 00:12:45 +0800

    store router learning

---
+++ src/store/modules/auth.js
@@ -0,0 +1,34 @@
+import api from '../../api/imgur'
+
+const state = {
+    token: null
+};
+
+const getters = {
+    /*isLoggedIn: (state) => {
+        return !!state.token
+    }*/
+    isLoggedIn: state => !!state.token
+};
+
+const actions = {
+    login: () => {
+        api.login()
+    },
+    logout: ({ commit }) => {
+        commit('setToken', null)
+    }
+};
+
+const mutations = {
+    setToken: (state, token) => {
+        state.token = token;
+    }
+};
+
+export default{
+    state,
+    getters,
+    actions,
+    mutations
+}
