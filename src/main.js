87e6c427d7c9750f7f813ab03c7979d033fd5c97
donna-chen <422194155@qq.com>
2020-03-30 22:22:15 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import VueRouter from 'vue-router'
 import App from './App';
 import store from './store';
 import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList';
+import UploadForm from './components/UploadForm';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
     mode: 'history',
     routes: [
+        { path:'/', component:ImageList},
+        { path:'/upload', component: UploadForm},
         { path: '/oauth2/calllback', component: AuthHandler }
     ]
 });
